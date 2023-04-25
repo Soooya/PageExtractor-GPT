4ff3f011f0e67db8e7324c5cc01e8e919e3a1096
linbo lai <6095420+Soooya@users.noreply.github.com>
2023-04-25 15:33:36 +0200

    init2

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,66 +1,60 @@
-$(document).ready(() => {
-    const listElement = $('#alarmsList');
-    
-    browser.storage.sync.get(['alarms'])
-        .then((result) => {
-            if (result.hasOwnProperty('alarms') && result.alarms) {
-                //get current time
-                const minutes = (new Date).getMinutes().toString().padStart(2, '0');
-                const hours = (new Date).getHours().toString().padStart(2, '0');
-                const now = new Date('1970-01-01T' + hours + ':' + minutes + 'Z').getTime();
-                //loop over the alarms and display them
-                result.alarms.forEach((alarm, index) => {
-                    const alarmTime = new Date('1970-01-01T' + alarm.time + 'Z').getTime();
-                    if (alarmTime > now) {
-                        appendItem(alarm.content, alarm.time, index);
-                    }
-                });
-            } else {
-                //show no items available
-                appendItem('No alarms are available');
-            }
-        });
 
-    $("#optionsLink").on('click', () => {
-        browser.runtime.openOptionsPage();
-    });
-
-    function appendItem (content, badgeContent = null, id = null) {
-        console.log(id)
-        listElement.append(`
-        <li class="list-group-item d-flex justify-content-between align-items-center alarm-item" ${id !== null ? `id="alarm_${id}"` : ''}>
-            ${content}
-            ${badgeContent ? `
-                <div>
-                    <span class="badge bg-primary rounded-pill">${badgeContent}</span>
-                    <button class="trash-btn p-0"><img src="assets/images/trash.svg" alt="delete" /></button>
-                </div>
-            ` : ''}
-        </li>
-        `);
+function reqListener () {
+    const response = JSON.parse(this.responseText);
+    if (response.message === "ok") {
+        on_ok();
+    } else {
+        on_fail(this.status);
     }
+}
 
-    $('body').on('click', '.trash-btn', function () {
-        const parent = $(this).parents('.alarm-item');
-        const parentId = parent.attr('id');
-        const alarmIndex = parentId.split('_')[1];
+function reqError () {
+    on_fail();
+    console.log('Fetch Error', arguments);
+}
 
-        //get alarms from storage
-        browser.storage.sync.get(['alarms'])
-            .then((result) => {
-                let alarms = [];
-                let alarmName = "";
-                if (result.alarms && result.alarms.length > alarmIndex) {
-                    alarmName = result.alarms[alarmIndex].alarmName;
-                    result.alarms.splice(alarmIndex, 1);
-                }
-                browser.storage.sync.set({alarms})
-                    .then(() => {
-                        //remove alarm by name
-                        browser.alarms.clear(alarmName);
-                        //remove alarm item from list
-                        parent.remove();
-                    });
-            });
-    });
+function reqTabs(tabs) {
+    let tab = tabs[0]; // Safe to assume there will only be one result
+    makeRequest(tab.url);
+}
+
+
+
+function makeRequest (url) {
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.onerror = reqError;
+    oReq.open('post', 'http://localhost:2861', true);
+    oReq.setRequestHeader('Content-Type', 'application/json');
+    oReq.setRequestHeader('Access-Control-Allow-Origin', '*');
+    oReq.send(JSON.stringify({"url": url }));
+}
+
+$(document).ready(() => {
+    browser.tabs.query({currentWindow: true, active: true}).then(reqTabs, console.error);
 });
+
+
+function on_ok() {
+    // Select the ul element with the id "alarmsList"
+    const alarmsList = document.querySelector('ul#alarmsList');
+    // Create a new li element
+    const newAlarm = document.createElement('li');
+    // Set the text of the new li element to the link sent
+    newAlarm.textContent = "Sent!!!";
+    // Append the new li element to the ul element
+    alarmsList.appendChild(newAlarm);
+}
+
+
+
+function on_fail(num) {
+    // Select the ul element with the id "alarmsList"
+    const alarmsList = document.querySelector('ul#alarmsList');
+    // Create a new li element
+    const newAlarm = document.createElement('li');
+    // Set the text of the new li element to the link sent
+    newAlarm.textContent = "Error!!"+String(num);
+    // Append the new li element to the ul element
+    alarmsList.appendChild(newAlarm);
+}
